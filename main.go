afecfbca469cdb8cca0b15ba08d4500f535cd189
Nate Jones <nate@endot.org>
2016-08-31 21:17:33 -0700

    first version of loading strategies
    * loading yaml
    * blending main info with versions
    * optional presence of arch map

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path"
+	"io/ioutil"
+	"log"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
-	fmt.Println(path.Base(os.Args[0]))
+	m := Manifest{}
+
+	data, err := ioutil.ReadFile("manifests/docker-compose.yaml")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	err = yaml.Unmarshal([]byte(data), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	// load this from config or by detecting environment
+	defaultStrategy := "docker"
+
+	strategy, err := loadStrategy(m, defaultStrategy, "")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	fmt.Printf("%v\n", strategy)
 }
