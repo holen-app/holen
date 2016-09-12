68ba283788025aa6c80128e17b3b591bd6e779ed
Nate Jones <nate@endot.org>
2016-09-12 07:49:10 -0700

    implement arch map, add jq manifest for testing

--- strategy.go
+++ strategy.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/pkg/errors"
+import (
+	"fmt"
+
+	"github.com/kr/pretty"
+	"github.com/pkg/errors"
+)
 
 type StrategyCommon struct {
 	System
@@ -11,12 +16,14 @@ type StrategyCommon struct {
 }
 
 func (sc *StrategyCommon) Templater(version string, archMap map[string]string, system System) Templater {
-	// TODO: extract MappedArch
+	archKey := fmt.Sprintf("%s_%s", system.OS(), system.Arch())
+	sc.Debugf("Arch key: %s", archKey)
+	value := archMap[archKey]
 	return Templater{
 		Version:    version,
 		OS:         system.OS(),
 		Arch:       system.Arch(),
-		MappedArch: "test",
+		MappedArch: value,
 	}
 }
 
@@ -56,6 +63,8 @@ func (ds DockerStrategy) Run(args []string) error {
 	var err error
 
 	temp := ds.Templater(ds.Data.Version, ds.Data.ArchMap, ds.System)
+	ds.Debugf("templater: %# v", pretty.Formatter(temp))
+
 	image, err := temp.Template(ds.Data.Image)
 	if err != nil {
 		return errors.Wrap(err, "unable to template image name")
@@ -75,5 +84,25 @@ func (ds DockerStrategy) Run(args []string) error {
 }
 
 func (bs BinaryStrategy) Run(args []string) error {
+	// var err error
+
+	temp := bs.Templater(bs.Data.Version, bs.Data.ArchMap, bs.System)
+	bs.Debugf("templater: %# v", pretty.Formatter(temp))
+
+	url, err := temp.Template(bs.Data.BaseUrl)
+	if err != nil {
+		return errors.Wrap(err, "unable to template url")
+	}
+
+	err = bs.DownloadFile(url, "local/jq")
+	if err != nil {
+		return errors.Wrap(err, "can't download binary")
+	}
+
+	bs.RunCommand("local/jq", args)
+	if err != nil {
+		return errors.Wrap(err, "can't run image")
+	}
+
 	return nil
 }
