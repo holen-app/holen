c0f8e3c8d347dc6ec0c532be014c1a5373927eb2
Nate Jones <nate@endot.org>
2016-09-22 06:10:39 -0700

    starting to add godoc

--- errors.go
+++ errors.go
@@ -1,5 +1,9 @@
 package main
 
+// SkipError can be returned from a Strategy to indicate that it is unable to
+// run the requested utility or version, but no actual error occurred.  For
+// instance, the Docker strategy could return this when Docker is not
+// installed.
 type SkipError struct {
 	Message string
 }
