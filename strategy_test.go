d47fa95b72a20edbbe16c08bd411c43f082a2b4d
Nate Jones <nate@endot.org>
2016-10-06 07:25:59 -0700

    add some tests for binary strategy

--- strategy_test.go
+++ strategy_test.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type DockerTestUtils struct {
+type TestUtils struct {
 	*MemSystem
 	*MemLogger
 	*MemConfig
@@ -17,8 +18,8 @@ type DockerTestUtils struct {
 	*MemRunner
 }
 
-func newDockerStrategy() (*DockerTestUtils, *DockerStrategy) {
-	tu := &DockerTestUtils{
+func newDockerStrategy() (*TestUtils, *DockerStrategy) {
+	tu := &TestUtils{
 		MemSystem:     &MemSystem{runtime.GOOS, runtime.GOARCH, 1000, 1000, make(map[string]bool)},
 		MemLogger:     &MemLogger{},
 		MemConfig:     &MemConfig{},
@@ -99,4 +100,114 @@ func TestDockerCommandFailed(t *testing.T) {
 	err := td.Run([]string{"first", "second"})
 	assert.NotNil(err)
 	// assert.Contains(err.Error(), "bad output")
+}
+
+func newBinaryStrategy() (*TestUtils, *BinaryStrategy) {
+	tu := &TestUtils{
+		MemSystem:     &MemSystem{runtime.GOOS, runtime.GOARCH, 1000, 1000, make(map[string]bool)},
+		MemLogger:     &MemLogger{},
+		MemConfig:     &MemConfig{},
+		MemDownloader: &MemDownloader{},
+		MemRunner:     &MemRunner{},
+	}
+	return tu, &BinaryStrategy{
+		StrategyCommon: &StrategyCommon{
+			System:       tu.MemSystem,
+			Logger:       tu.MemLogger,
+			ConfigGetter: tu.MemConfig,
+			Downloader:   tu.MemDownloader,
+			Runner:       tu.MemRunner,
+		},
+		Data: BinaryData{
+			Name:      "testbinary",
+			Desc:      "Test Binary Program",
+			Version:   "2.1",
+			BaseURL:   "https://github.com/testbinary/bin/releases/download/bin-{{.Version}}/jq-{{.OSArch}}",
+			OSArchMap: make(map[string]string),
+		},
+	}
+}
+
+func TestBinarySimple(t *testing.T) {
+
+	assert := assert.New(t)
+
+	tu, tb := newBinaryStrategy()
+	tb.Run([]string{"first", "second"})
+
+	binPath := path.Join(os.Getenv("HOME"), ".local/share/holen/bin/testbinary--2.1")
+	remoteUrl := "https://github.com/testbinary/bin/releases/download/bin-2.1/jq-linux_amd64"
+
+	// check download
+	assert.Contains(tu.MemDownloader.Files, remoteUrl)
+	assert.Equal(tu.MemDownloader.Files[remoteUrl], binPath)
+
+	assert.Equal(tu.MemRunner.History[0], fmt.Sprintf("%s first second", binPath))
+}
+
+func TestBinaryBadImageTemplate(t *testing.T) {
+	assert := assert.New(t)
+
+	_, tb := newBinaryStrategy()
+	tb.Data.BaseURL = "https://{{.Foo"
+
+	err := tb.Run([]string{"first", "second"})
+	assert.NotNil(err)
+	assert.Contains(err.Error(), "unclosed action")
+}
+
+func TestBinaryDownloadPath(t *testing.T) {
+	assert := assert.New(t)
+
+	var downloadPathTests = []struct {
+		adjustment func(*TestUtils)
+		err        error
+		result     string
+		cleanup    func(*TestUtils)
+	}{
+		{
+			nil,
+			nil,
+			path.Join(os.Getenv("HOME"), ".local/share/holen/bin"),
+			nil,
+		},
+		{
+			func(tu *TestUtils) {
+				os.Setenv("XDG_DATA_HOME", "/tmp")
+			},
+			nil,
+			"/tmp/holen/bin",
+			func(tu *TestUtils) {
+				os.Setenv("XDG_DATA_HOME", "")
+			},
+		},
+		{
+			func(tu *TestUtils) {
+				os.Setenv("HOME", "")
+			},
+			fmt.Errorf("$HOME not found"),
+			"",
+			func(tu *TestUtils) { return },
+		},
+	}
+
+	for _, test := range downloadPathTests {
+
+		tu, tb := newBinaryStrategy()
+		if test.adjustment != nil {
+			test.adjustment(tu)
+		}
+
+		result, err := tb.DownloadPath()
+		if test.err == nil {
+			assert.Nil(err)
+		} else {
+			assert.NotNil(err)
+		}
+		assert.Equal(result, test.result)
+		if test.cleanup != nil {
+			test.cleanup(tu)
+		}
+	}
+
 }
