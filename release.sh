c311881a67557eac289be0609605d25812e3422a
Nate Jones <nate@endot.org>
2024-03-19 19:49:14 -0700

    Switch to gh for releases, add arm Mac build

--- release.sh
+++ release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [[ ! $(type -P gox) ]]; then
     echo "Error: gox not found."
-    echo "To fix: run 'go get github.com/mitchellh/gox', and/or add \$GOPATH/bin to \$PATH"
+    echo "To fix: run 'go install github.com/mitchellh/gox@latest', and/or add \$GOPATH/bin to \$PATH"
     exit 1
 fi
 
-if [[ ! $(type -P github-release) ]]; then
-    echo "Error: github-release not found."
+if [[ ! $(type -P gh) ]]; then
+    echo "Error: github cli not found."
     exit 1
 fi
 
@@ -30,7 +30,7 @@ git tag $VER
 echo "Building $VER"
 echo
 
-gox -ldflags "-X main.version=$VER" -osarch="darwin/amd64 linux/amd64 windows/amd64 linux/arm"
+gox -ldflags "-X main.version=$VER" -osarch="darwin/amd64 linux/amd64 windows/amd64 linux/arm darwin/arm64"
 
 echo "* " > desc
 echo "" >> desc
@@ -52,9 +52,4 @@ git push --tags
 
 sleep 2
 
-cat desc | github-release release $PRE_ARG --user holen-app --repo holen --tag $VER --name $VER --description -
-github-release upload --user holen-app --repo holen --tag $VER --name holen_darwin_amd64 --file holen_darwin_amd64
-github-release upload --user holen-app --repo holen --tag $VER --name holen_linux_amd64 --file holen_linux_amd64
-github-release upload --user holen-app --repo holen --tag $VER --name holen_linux_arm --file holen_linux_arm
-github-release upload --user holen-app --repo holen --tag $VER --name holen_windows_amd64.exe --file holen_windows_amd64.exe
-github-release upload --user holen-app --repo holen --tag $VER --name holen.bootstrap --file holen.bootstrap
+gh release create $VER -t $VER -F desc holen_* holen.bootstrap
